badbc79f1af4cccc0dbc31c9e8923c103fbf94ef
Patrick Dubroy <pdubroy@gmail.com>
2025-01-22 22:39:26 +0100

    Implement 16-bit registers.

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,52 @@
 const std = @import("std");
 
+const Registers = struct {
+    a: u8,
+    b: u8,
+    c: u8,
+    d: u8,
+    e: u8,
+    f: u8,
+    h: u8,
+    l: u8,
+
+    pub fn getBC(self: *const Registers) u16 {
+        return (@as(u16, self.b) << 8) | self.c;
+    }
+
+    pub fn setBC(self: *Registers, value: u16) void {
+        self.b = @truncate((value >> 8) & 0xFF);
+        self.c = @truncate(value & 0xFF);
+    }
+
+    pub fn getAF(self: *const Registers) u16 {
+        return (@as(u16, self.a) << 8) | self.f;
+    }
+
+    pub fn setAF(self: *Registers, value: u16) void {
+        self.a = @truncate((value >> 8) & 0xFF);
+        self.f = @truncate(value & 0xFF);
+    }
+
+    pub fn getDE(self: *const Registers) u16 {
+        return (@as(u16, self.d) << 8) | self.e;
+    }
+
+    pub fn setDE(self: *Registers, value: u16) void {
+        self.d = @truncate((value >> 8) & 0xFF);
+        self.e = @truncate(value & 0xFF);
+    }
+
+    pub fn getHL(self: *const Registers) u16 {
+        return (@as(u16, self.h) << 8) | self.l;
+    }
+
+    pub fn setHL(self: *Registers, value: u16) void {
+        self.h = @truncate((value >> 8) & 0xFF);
+        self.l = @truncate(value & 0xFF);
+    }
+};
+
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
@@ -21,4 +68,25 @@ test "simple test" {
     defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
     try list.append(42);
     try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
+
+test "registers" {
+    var regs = Registers{
+        .a = 0x12,
+        .b = 0x34,
+        .c = 0x56,
+        .d = 0x78,
+        .e = 0x9A,
+        .f = 0xBC,
+        .h = 0xDE,
+        .l = 0xF0,
+    };
+    try std.testing.expectEqual(regs.getBC(), 0x3456);
+    try std.testing.expectEqual(regs.getDE(), 0x789A);
+    try std.testing.expectEqual(regs.getAF(), 0x12BC);
+
+    regs.a = 1;
+    try std.testing.expectEqual(regs.a, 1);
+    regs.l = 99;
+    try std.testing.expectEqual(regs.l, 99);
 }
