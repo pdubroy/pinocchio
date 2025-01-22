9f2a08e60eb7490905c86cdb241e7d10761bf687
Patrick Dubroy <pdubroy@gmail.com>
2025-01-22 22:59:17 +0100

    Implement flags register

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,14 @@
 const std = @import("std");
 
 const Registers = struct {
-    a: u8,
-    b: u8,
-    c: u8,
-    d: u8,
-    e: u8,
-    f: u8,
-    h: u8,
-    l: u8,
+    a: u8 = 0,
+    b: u8 = 0,
+    c: u8 = 0,
+    d: u8 = 0,
+    e: u8 = 0,
+    f: u8 = 0,
+    h: u8 = 0,
+    l: u8 = 0,
 
     pub fn getBC(self: *const Registers) u16 {
         return (@as(u16, self.b) << 8) | self.c;
@@ -47,6 +47,34 @@ const Registers = struct {
     }
 };
 
+const ZERO_FLAG_BYTE_POSITION: u8 = 7;
+const SUBTRACT_FLAG_BYTE_POSITION: u8 = 6;
+const HALF_CARRY_FLAG_BYTE_POSITION: u8 = 5;
+const CARRY_FLAG_BYTE_POSITION: u8 = 4;
+
+const FlagsRegister = struct {
+    zero: bool,
+    subtract: bool,
+    half_carry: bool,
+    carry: bool,
+
+    pub fn toU8(self: FlagsRegister) u8 {
+        return (@as(u8, if (self.zero) 1 else 0) << ZERO_FLAG_BYTE_POSITION) |
+            (@as(u8, if (self.subtract) 1 else 0) << SUBTRACT_FLAG_BYTE_POSITION) |
+            (@as(u8, if (self.half_carry) 1 else 0) << HALF_CARRY_FLAG_BYTE_POSITION) |
+            (@as(u8, if (self.carry) 1 else 0) << CARRY_FLAG_BYTE_POSITION);
+    }
+
+    pub fn fromU8(byte: u8) FlagsRegister {
+        return FlagsRegister{
+            .zero = ((byte >> ZERO_FLAG_BYTE_POSITION) & 0b1) != 0,
+            .subtract = ((byte >> SUBTRACT_FLAG_BYTE_POSITION) & 0b1) != 0,
+            .half_carry = ((byte >> HALF_CARRY_FLAG_BYTE_POSITION) & 0b1) != 0,
+            .carry = ((byte >> CARRY_FLAG_BYTE_POSITION) & 0b1) != 0,
+        };
+    }
+};
+
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
@@ -70,7 +98,15 @@ test "simple test" {
     try std.testing.expectEqual(@as(i32, 42), list.pop());
 }
 
-test "registers" {
+test "8-bit registers" {
+    var regs = Registers{};
+    regs.a = 1;
+    try std.testing.expectEqual(regs.a, 1);
+    regs.l = 99;
+    try std.testing.expectEqual(regs.l, 99);
+}
+
+test "16-bit registers" {
     var regs = Registers{
         .a = 0x12,
         .b = 0x34,
@@ -84,9 +120,15 @@ test "registers" {
     try std.testing.expectEqual(regs.getBC(), 0x3456);
     try std.testing.expectEqual(regs.getDE(), 0x789A);
     try std.testing.expectEqual(regs.getAF(), 0x12BC);
+}
 
-    regs.a = 1;
-    try std.testing.expectEqual(regs.a, 1);
-    regs.l = 99;
-    try std.testing.expectEqual(regs.l, 99);
+test "flags register" {
+    var flags = FlagsRegister{
+        .zero = true,
+        .subtract = false,
+        .half_carry = true,
+        .carry = false,
+    };
+    try std.testing.expectEqual(flags.toU8(), 0b10100000);
+    try std.testing.expectEqual(flags, FlagsRegister.fromU8(0b10100000));
 }
